e60851848a2dc38787b578fa151ed8b1f96fb4fe
Mauricio Rodriguez (w) <maurijrp2001@gmail.com>
2021-03-18 17:25:38 -0600

    Ipv4

---
+++ sniffer.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <bitset>
+#include <cstring>
+
+using namespace std;
+
+void ipv4()
+{
+  unsigned char charBuffer;
+  int byteInt;
+  string byteString;
+  fstream file("./paquetesRedes/ethernet_ipv4_icmp.bin", ios::in);
+  file.seekg(14, ios::beg);
+
+  // -------- ------- ------ ----- Version and Header Size----- ------ ------- --------
+  file.read((char *)&charBuffer, sizeof(charBuffer));
+  byteInt = static_cast<int>(charBuffer);
+  byteString = bitset<8>(byteInt).to_string();
+
+  string version = byteString.substr(0, 4);
+  cout << "Version: " << stoi(version, 0, 2) << '\n';
+
+  string headerSize = byteString.substr(4, 8);
+  cout << "Tamano de cabecera: " << stoi(headerSize, 0, 2) << " Palabras" << '\n';
+
+  // -------- ------- ------ ----- Service ----- ------ ------- --------
+  file.read((char *)&charBuffer, sizeof(charBuffer));
+  byteInt = static_cast<int>(charBuffer);
+  byteString = bitset<8>(byteInt).to_string();
+
+  string priority = byteString.substr(0, 3);
+  if (priority == "000")
+    priority = "De rutina";
+  else if (priority == "001")
+    priority = "Prioritario";
+  else if (priority == "010")
+    priority = "Inmediato";
+  else if (priority == "011")
+    priority = "Relampago";
+  else if (priority == "100")
+    priority = "Invalidacion relampago";
+  else if (priority == "101")
+    priority = "Procesando llamada critica y de emergencia";
+  else if (priority == "110")
+    priority = "Control de trabajo de internet";
+  else
+    priority = "Control de red";
+
+  cout << "Prioridad: " << priority;
+  cout << "\nRetardo: " << ((byteString[3] == '0') ? "normal" : "bajo");
+  cout << "\nRendimiento: " << ((byteString[4] == '0') ? "normal" : "alto");
+  cout << "\nFiabilidad: " << ((byteString[5] == '0') ? "normal" : "alta") << '\n';
+}
+
+int main()
+{
+  string bytes;
+  unsigned char byte;
+  int count = 0;
+  string macDestino;
+  string macOrigen;
+  string tipo;
+  string datos;
+
+  stringstream ss;
+  fstream archivo("./paquetesRedes/ethernet_ipv4_icmp.bin", ios::in);
+
+  if (!archivo.is_open())
+  {
+    cout << "error" << endl;
+  }
+  else
+  {
+    while (archivo >> byte)
+    {
+      ++count;
+      ss << setw(2) << setfill('0') << hex << static_cast<int>(byte);
+      if (count == 6)
+      {
+        ss >> macDestino;
+        ss.clear();
+        cout << "MAC Destino: " << macDestino << endl;
+      }
+      else if (count == 12)
+      {
+        ss >> macOrigen;
+        ss.clear();
+        cout << "MAC Origen: " << macOrigen << endl;
+      }
+      else if (count == 14)
+      {
+        ss >> tipo;
+        cout << "Tipo: " << tipo << " ";
+
+        if (tipo == "0800")
+        {
+          cout << "IPv4" << endl;
+          archivo.close();
+          ipv4();
+          break;
+        }
+        else if (tipo == "0806")
+          cout << "ARP" << endl;
+        else if (tipo == "8035")
+          cout << "RARP" << endl;
+        else if (tipo == "86DD")
+          cout << "IPv6" << endl;
+        else
+        {
+          cout << "El IP no está contemplado.";
+        }
+
+        ss.clear();
+      }
+      else if (count < 12)
+      {
+        ss << ":";
+      }
+    }
+    ss >> datos;
+    ss.clear();
+    // cout << "Datos: " << datos << endl;
+  }
+  archivo.close();
+  return 0;
+}
--- test.cpp
+++ test.cpp
@@ -9,20 +9,87 @@ int main()
 
   FILE *archivo;
   int counter{0};
+  string code{""};
+  unsigned int code1;
+  unsigned int code2;
   if ((archivo = fopen("./paquetesRedes/ethernet_1.bin", "rb")) == NULL)
   {
     cout << "Error en la apertura. Es posible que el fichero no exista \n";
   }
   else
   {
-    while (!feof(archivo) and counter < 14)
+    while (!feof(archivo))
     {
       car = fgetc(archivo);
+      if (counter == 0)
+      {
+        code1 = static_cast<int>(car);
+
+        cout << "Direccion MAC destino: ";
+      }
+      if (counter == 6)
+      {
+        cout << "\nDireccion MAC origen: ";
+      }
+      if (counter == 12)
+      {
+        cout << "\nTipo de codigo: ";
+      }
+      if (counter == 13)
+      {
+        code2 = static_cast<int>(car);
+      }
+      if (counter == 14)
+      {
+        cout << "\nData: ";
+      }
       if (car < 9)
         cout << '0';
-      cout << hex << static_cast<int>(car) << " ";
+      cout << hex << static_cast<int>(car) << ":";
       ++counter;
     }
     fclose(archivo);
+    code = (code1 < 9 ? '0' + to_string(code1) : to_string(code1)) + (code2 < 9 ? '0' + to_string(code2) : to_string(code2));
+    cout << "\ncodigo: " << code;
+    int test = std::stoi(code);
+    cout << "prueba: " << test;
+    // switch (test)
+    // {
+    // case '':
+    //   /* code */
+    //   break;
+
+    // default:
+    //   break;
+    // }
   }
 }
+
+// std::cout << MACAddressDestino;
+
+// while (!feof(archivo))
+// {
+//   car = fgetc(archivo);
+//   if (car < 9)
+//     std::cout << '0';
+//   if (counter < 6)
+//   {
+//     MACAddressDestino += car;
+//   }
+//   else if (counter > 5 and counter < 12)
+//   {
+//     // MACAddressOrigen += static_cast<int>(car);
+//     std::cout << hex << static_cast<int>(car) << " ";
+//   }
+//   else if (counter > 12 and counter < 14)
+//   {
+//     // code += static_cast<int>(car);
+//     std::cout << hex << static_cast<int>(car) << " ";
+//   }
+//   else
+//   {
+//     // data += static_cast<int>(car);
+//     std::cout << hex << static_cast<int>(car) << " ";
+//   }
+//   ++counter;
+// }
