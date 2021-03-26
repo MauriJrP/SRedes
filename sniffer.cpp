21f9b2ff95c08efe102b1e7510fbf1c5e36e1d85
Mauricio Rodriguez (w) <maurijrp2001@gmail.com>
2021-03-26 14:51:41 -0600

    ICMPv4

--- sniffer.cpp
+++ sniffer.cpp
@@ -7,9 +7,99 @@
 
 using namespace std;
 
+void ICMPv4()
+{
+  cout << "-------- ------- ------ ----- ICMPv4 ----- ------ ------- --------\n";
+  int byteInt;
+  string byteString;
+  unsigned char charArray[4];
+  fstream file("./paquetesRedes/ethernet_ipv4_icmp.bin", ios::in);
+  file.seekg(34, ios::beg);
+  file.read((char *)&charArray, sizeof(charArray));
+
+  // -------- ------- ------ ----- Type ----- ------ ------- --------
+
+  byteInt = static_cast<int>(charArray[0]);
+  byteString = bitset<8>(byteInt).to_string();
+  int type = stoi(byteString, 0, 2);
+  cout << "Tipo: " << type << " ";
+
+  if (type == 0)
+    cout << "Respuesta de eco";
+  else if (type == 3)
+    cout << "Destino inaccesible";
+  else if (type == 4)
+    cout << "Disminucion de trafico desde el origen";
+  else if (type == 5)
+    cout << "Redireccionar - cambio de ruta";
+  else if (type == 8)
+    cout << "Solicitud de eco";
+  else if (type == 11)
+    cout << "Tiempo excedido para un datagrama";
+  else if (type == 12)
+    cout << "Problema de parametros";
+  else if (type == 13)
+    cout << "Solicitud de marca de tiempo";
+  else if (type == 14)
+    cout << "Respuesta de marca de tiempo";
+  else if (type == 15)
+    cout << "Solicitud de informacion";
+  else if (type == 16)
+    cout << "Respuesta de informacion";
+  else if (type == 17)
+    cout << "Solicutod de mascara de direccion";
+  else if (type == 18)
+    cout << "Respuesta de mascara de direccion";
+
+  // -------- ------- ------ ----- Code ----- ------ ------- --------
+  byteInt = static_cast<int>(charArray[1]);
+  byteString = bitset<8>(byteInt).to_string();
+  int code = stoi(byteString, 0, 2);
+  cout << "\nCodigo: " << code << " ";
+
+  if (code == 0)
+    cout << "No se puede llegar a la red";
+  else if (code == 1)
+    cout << "No se puede llegar al host o aplicacion de destino";
+  else if (code == 2)
+    cout << "El destino no se dispone del protocolo solicitado";
+  else if (code == 3)
+    cout << "No se puede llegar al puerto destino a la aplicacion destino no esta libre";
+  else if (code == 4)
+    cout << "Se necesita aplicar fragmentacion, pero el flag correspondiente indica lo contrario";
+  else if (code == 5)
+    cout << "La ruta de origen no es correcta";
+  else if (code == 6)
+    cout << "No se conoce la red destino";
+  else if (code == 7)
+    cout << "No se conoce el host destino";
+  else if (code == 8)
+    cout << "El host origen esta aislado";
+  else if (code == 9)
+    cout << "La comunicacion con la red destino esta prohibida por razones administrativas";
+  else if (code == 10)
+    cout << "La comunicacion con el host destino esta prohibida por razones administrativas";
+  else if (code == 11)
+    cout << "No se puede llegar a la red destino debido al Tipo de servicio";
+  else if (code == 12)
+    cout << "No se puede llegar al host destino debido al Tipo de servicio";
+
+  // -------- ------- ------ ----- Checksum ----- ------ ------- --------
+  byteInt = static_cast<int>(charArray[2]);
+  byteString = bitset<8>(byteInt).to_string();
+  byteInt = static_cast<int>(charArray[3]);
+  byteString += bitset<8>(byteInt).to_string();
+
+  stringstream ss;
+  ss << setfill('0') << hex << stoi(byteString, 0, 2);
+  string checksum;
+  ss >> checksum;
+  cout << "\nChecksum: " << checksum << '\n';
+}
+
 void ipv4()
 {
-  unsigned char charBuffer;
+  cout << "-------- ------- ------ ----- ipv4 ----- ------ ------- --------\n";
   int byteInt;
   unsigned char charArray[20];
   string byteString;
@@ -74,9 +164,9 @@ void ipv4()
   byteString = bitset<8>(byteInt).to_string();
 
   cout << "Flags: \n";
-  cout << "Reservado\n";
-  cout << ((byteString[1] == '0') ? "Divisible" : "No divisible") << '\n';
-  cout << ((byteString[2] == '0') ? "Ultimo Fragmento" : "Fragmento Intermedio") << '\n';
+  cout << "  Reservado\n";
+  cout << ((byteString[1] == '0') ? "  Divisible" : "  No divisible") << '\n';
+  cout << ((byteString[2] == '0') ? "  Ultimo Fragmento" : "  Fragmento Intermedio") << '\n';
 
   // -------- ------- ------ ----- Fragment Position ----- ------ ------- --------
   byteString = byteString.substr(3, 5);
@@ -146,6 +236,10 @@ void ipv4()
   }
 
   cout << "Direccion IP de Destino: " << ipAddressDestination << '\n';
+
+  // -------- ------- ------ ----- Protocol ----- ------ ------- --------
+  if (protocol == 1)
+    ICMPv4();
 }
 
 int main()
@@ -208,7 +302,7 @@ int main()
 
         ss.clear();
       }
-      else if (count == 34)
+      else if (count == 38)
       {
         string test;
         ss >> test;
